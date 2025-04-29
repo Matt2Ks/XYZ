ac2f1d92dbba9f503ce6810d183e85b977c1831f
Matt2Ks <mateusz.ficner@creativemood.pl>
2025-04-29 19:34:55 +0200

    main.cjs

--- main.cjs
+++ main.cjs
@@ -1,6 +1,8 @@
-
-import { Actor } from 'apify';
+const { Actor } = require('apify');
+const someModule = require('some-module'); // Przykładowy dodatkowy import
 
 Actor.main(async () => {
-    console.log('Actor started...');
+    // Przykładowa logika
+    console.log('Actor is running');
+    // Dalsza logika Twojego skryptu
 });
